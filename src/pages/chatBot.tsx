79a59649fe2586d6fda3db985449e545c9ace379
suthakaranburaj <suthakaranburaj@gmail.com>
2025-02-22 15:46:09 +0530

    toggle effect added for chatSection

--- src/pages/chatBot.tsx
+++ src/pages/chatBot.tsx
@@ -22,7 +22,14 @@ function ChatBot() {
   }, []);
 
   return (
-    <div ref={chatBoxRef} style={{ width: "100%", height: "100%" }}>
+    <div
+      ref={chatBoxRef}
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "relative",
+      }}
+    >
       <div
         className={`transition-all duration-300 ease-in-out overflow-hidden`}
         style={{ width: isOpen ? 300 : 0 }}
@@ -30,13 +37,25 @@ function ChatBot() {
         <components.VirtualizedList
           height={height}
           width={300}
-          itemSize={50}
+          // itemSize={50}
           itemCount={100}
           overscanCount={10}
         />
       </div>
-      <Button onClick={() => setIsOpen(!isOpen)} variant="outlined">
-        Primary
+      <Button
+        onClick={() => setIsOpen(!isOpen)}
+        variant="outlined"
+        style={{
+          position: "absolute",
+          left: isOpen ? 300 : 0, 
+          top: "5%",
+          transform: "translateY(-50%)",
+          zIndex: 1,
+          minWidth: "auto",
+          transition: "left 0.3s ease-in-out",
+          border:"none"
+        }}
+      >
         {isOpen ? <PanelRightOpen /> : <PanelLeftOpen />}
       </Button>
     </div>

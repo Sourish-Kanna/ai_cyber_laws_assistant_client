f11f37174cbc2d54201d3a3dd785dd2cdb750a5d
suthakaranburaj <suthakaranburaj@gmail.com>
2025-02-22 16:18:43 +0530

    imports setup

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export { default as ChatBot } from "./pages/chatBot";
+export { default as ChatBot } from "./pages/ChatBot/chatBot.tsx";
 export { default as Dashboard } from "./pages/Dashboard";
-export {default as App } from './App.tsx'
+export { default as App } from "./App.tsx";
+export {default as ChattingPage } from './pages/ChatBot/chattingPage.tsx'
---
+++ src/pages/ChatBot/chattingPage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface chattingPageProps {
+  width?: Number;
+}
+
+function chattingPage({ width = 100 }: chattingPageProps) {
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  );
+}
+
+export default chattingPage;

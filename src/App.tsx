f718521da0008ce00b40310821478dc94fcfb51a
Sourish Kanna <sourishkanna001@gmail.com>
2025-04-06 23:58:34 +0530

    add authentication pages; implement login, logout, and register functionalities with error handling

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { createTheme } from "@mui/material/styles";
 import DashboardIcon from "@mui/icons-material/Dashboard";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+// import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import BarChartIcon from "@mui/icons-material/BarChart";
 import DescriptionIcon from "@mui/icons-material/Description";
 import LayersIcon from "@mui/icons-material/Layers";
@@ -18,7 +18,7 @@ import {
   SidebarFooterProps,
   ThemeSwitcher,
 } from "@toolpad/core/DashboardLayout";
-import { useDemoRouter } from "@toolpad/core/internal";
+// import { useDemoRouter } from "@toolpad/core/internal";
 import {
   Avatar,
   Chip,
